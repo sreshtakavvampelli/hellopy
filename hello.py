6034024176c34769176010e604b3aab1e2705d5b
sreshtakavvampelli <kavvampellisreshta11@gmail.com>
2025-09-23 10:59:17 +0530

    break command is added

---
+++ hello.py
@@ -0,0 +1,7 @@
+# Simple Python program to greet the user
+
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Print a greeting
+print(f"Hello, {name}! Welcome to the Python world!")
